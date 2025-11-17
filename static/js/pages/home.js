25111fbc8f165d34c62d965d07bbc1d1c62ec710
= <=>
2025-11-17 21:19:44 +0530

    Updated skills section

--- static/js/pages/home.js
+++ static/js/pages/home.js
@@ -1,10 +1,3 @@
-// ? GETTING SKILLS ELEMENTS
-const skillBars = document.querySelectorAll('.skill-display');
-const frontendCont = document.querySelector('.desc#frontend');
-const backendCont = document.querySelector('.desc#backend');
-const DSCont = document.querySelector('.desc#data-science');
-const hoverContent = document.querySelector('#hover');
-
 // & TYPING ANIMATION SCRIPT FOR HERO SECTION
 var typed = new Typed('#type-1', {
     strings: ['Hi,'],
@@ -25,13 +18,7 @@ var typed = new Typed('#type-1', {
                         var typed3 = new Typed("#type-3", {
                             strings: ["A dedicated software developer &amp; entrepreneur."],
                             typeSpeed: 30,
-                            showCursor: false,
-
-                            // onComplete: function() {
-                            //     setTimeout(() => {
-                            //         render_skill_chart();
-                            //     }, 1000);
-                            // }
+                            showCursor: false
                         })
                     }, 1000);
                 }
@@ -40,76 +27,22 @@ var typed = new Typed('#type-1', {
     }
 });
 
-// * SKILLBARS DISPLAY ANIMATION SCRIPT
-function displaySkillBars(index=0) {
-    if (index >= skillBars.length) {return;}
+// // & ELEMENT OBSERVER SCRIPT
+// const observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             if (entry.target.classList.contains('skills')) {
+//                 displaySkillBars();
+//             }
+//         }
+//     });
+// }, {
+//     root: null,
+//     threshold: 0.2
+// });
 
-    skillBars[index].classList.add('show');
-
-    setTimeout(() => {
-        displaySkillBars(index+1);
-    }, 100);
-}
-
-// & ELEMENT OBSERVER SCRIPT
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            if (entry.target.classList.contains('skills')) {
-                displaySkillBars();
-            }
-        }
-    });
-}, {
-    root: null,
-    threshold: 0.2
-});
-
-// & SCRIPT TO USE THE OBSERVER TO OBSERVE .SECTION
-sections = document.querySelectorAll(".main section");
-sections.forEach((section) => {
-    observer.observe(section);
-})
-
-// * FUNCTION TO HIDE SKILL DESCRIPTIONS
-function hideSkillDesc() {
-    hoverContent.style.display = "none";
-    frontendCont.style.display = "none";
-    backendCont.style.display = "none";
-    DSCont.style.display = "none";
-
-    hoverContent.style.opacity = "0";
-    frontendCont.style.opacity = "0";
-    backendCont.style.opacity = "0";
-    DSCont.style.opacity = "0";
-}
-
-// & EVENT LISTENERS FOR SKILLBAR CLICKS
-skillBars.forEach((bar) => {
-    bar.addEventListener('click', () => {
-        hideSkillDesc();
-        if (bar.classList.contains('frontend')) {
-            frontendCont.style.display = "flex";
-
-            setTimeout(() => {
-                frontendCont.style.opacity = "1";
-            }, 100);
-        }
-
-        if (bar.classList.contains('backend')) {
-            backendCont.style.display = "flex";
-
-            setTimeout(() => {
-                backendCont.style.opacity = "1";
-            }, 100);
-        }
-
-        if (bar.classList.contains('data-science')) {
-            DSCont.style.display = "flex";
-
-            setTimeout(() => {
-                DSCont.style.opacity = "1";
-            }, 100);
-        }
-    })
-});
+// // & SCRIPT TO USE THE OBSERVER TO OBSERVE .SECTION
+// sections = document.querySelectorAll(".main section");
+// sections.forEach((section) => {
+//     observer.observe(section);
+// })
