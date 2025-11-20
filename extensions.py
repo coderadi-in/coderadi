cfb160de3074232be02b2feffe73ff7fa071ef99
= <=>
2025-11-20 20:57:32 +0530

    Updated site extensions

--- extensions.py
+++ extensions.py
@@ -25,3 +25,31 @@ class ContactDetails(db.Model):
     email = db.Column(db.String(75), nullable=False)
     subject = db.Column(db.TEXT)
     message = db.Column(db.TEXT, nullable=False)
+
+# | PROJECT DETAILS DATABASE MODEL
+class Project(db.Model):
+    '''
+    ###### coderadi
+    Holds the details of project built by coderadi.
+
+    ## Params
+    - `id` [`primary_key`, `String(100)`]: Id of row.
+    - `cover` [`String(100)`, `required`]: Cover image of project.
+    - `title` [`String(100)`, `required`]: Title of project.
+    - `desc` [`TEXT`, `required`]: Description of project.
+    - `category` [`String(50)`, `required`]: Category of project.
+    - `pre_release` [Boolean, `default=False`]: Is project pre-release.
+    - `external` [Boolean, `default=False`]: Is project external.
+    - `tech_stack` [`JSON`, `required`]: Technologies used in project.
+    - `link` [`String(100)`]: Link to project.
+    '''
+
+    id = db.Column(db.String(100), primary_key=True)
+    cover = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.TEXT, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    pre_release = db.Column(db.Boolean, default=False)
+    external = db.Column(db.Boolean, default=False)
+    tech_stack = db.Column(db.JSON, nullable=False)
+    link = db.Column(db.String(100))
--- router.py
+++ router.py
@@ -2,6 +2,7 @@
 
 # ? IMPORTING LIBRARIES
 from flask import Blueprint, redirect, url_for, render_template, flash, request
+from extensions import *
 
 # ! BUILDING ROUTER
 router = Blueprint('router', __name__)
@@ -17,6 +18,20 @@ def home():
 def projects():
     flash("This site is under development, some functions may not work", "warning")
     return render_template('pages/projects.html')
+
+# | SPECIFIC PROJECT ROUTE
+@router.route('/projects/<project_id>')
+def show_project(project_id):
+    project = Project.query.filter_by(id=project_id).first()
+    
+    if (not project):
+        flash("Project not found", "error")
+        return redirect(url_for('router.projects'))
+    
+    if (not project.external):
+        return render_template(project.link)
+    
+    return redirect(project.link)
 
 # & ABOUT PAGE ROUTE
 @router.route('/about/')
