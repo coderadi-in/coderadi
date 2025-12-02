fced8c83980b336a916da33574cfa6689f15f307
= <=>
2025-12-02 10:05:30 +0530

    Created a test route for api.

--- api.py
+++ api.py
@@ -3,7 +3,7 @@
 # ? IMPORTING LIBRARIES
 from flask import Blueprint, request
 from extensions import db, Project, notify
-import os
+import os, json
 
 # ! BUILDING API ROUTER
 api = Blueprint('api', __name__, url_prefix='/api')
@@ -40,6 +40,72 @@ def get_db_list():
             'url': project.url
         } for project in projects]
     }, 200
+
+# | DB-PROJECT `NEW` ROUTE [disk testing]
+@api.route('/db-test/projects/new', methods=['POST'])
+def new_db_test_project():
+    '''Create a new prject in disk'''
+
+    content_type = request.headers.get('Content-Type')
+    if (content_type != 'application/json'):
+        return {
+            'res': {
+                'status': 415,
+                'message': 'Unsupported Media Type: Content-Type must be application/json',
+            }
+        }, 415
+    
+    passkey = request.headers.get('Admin-Key')    
+    if (not check_admin_auth(passkey)):
+        return {
+            'res': {
+                'status': 403,
+                'message': 'Forbidden: Invalid Admin Key',
+            }
+        }, 403
+    
+    data = request.json
+    new_project = {
+        'id': data.get('id'),
+        'cover': data.get('cover'),
+        'title': data.get('title'),
+        'desc': data.get('desc'),
+        'category': data.get('category'),
+        'pre_release': data.get('pre_release', False),
+        'external': data.get('external', False),
+        'tech_stack': data.get('tech_stack'),
+        'url': data.get('url')
+    }
+
+    if (new_project['external']):
+        url_template = new_project['url']
+    else:
+        url_template = f"https://coderadi.in/projects/{new_project['url']}.html"
+
+    try:
+        # db.session.add(new_project)
+        # db.session.commit()
+
+        with open('db/projects.json', 'w') as f:
+            json.dump(new_project, f, indent=4)
+
+        notify(f"New project added to `portfolio`.\n\n{url_template}\n\n    — coderadi.in")
+
+        return {
+            'res': {
+                'status': 200,
+                'message': 'Project created successfully!',
+            }
+        }, 200
+    
+    except Exception as e:
+        # db.session.rollback()
+        return {
+            'res': {
+                'status': 500,
+                'message': f'Internal Server Error: {str(e)}',
+            }
+        }, 500
 
 # & DB-PROJECT `NEW` ROUTE
 @api.route('/db/projects/new', methods=['POST'])
